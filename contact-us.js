1375fd8c1d7e340be1026be8996af19ae5e7336d
Storm OBryant <vmarie1997@gmail.com>
2022-06-20 20:18:19 -0500

    it works

--- contact-us.js
+++ contact-us.js
@@ -1,5 +1,5 @@
 //grab contact form from html
-// let contactUsForm = document.getElementById('contact-us-form');
+let contactUsForm = document.getElementById('contact-us-form');
 
 
 // function printSubmittedForms(e){
@@ -23,45 +23,39 @@
 // //function being called = registerAndPrint function
 // document.querySelector('form').addEventListener('submit', printSubmittedForms);
 
-function getData(){
-    //get form values
-    let name = document.getElementById("enter-name").value;
-    let email= document.getElementById("e-mail").value; 
-    let message= document.getElementById("message").value;
+// function getData(){
+//     //get form values
+//     let name = document.getElementById("enter-name").value;
+//     let email= document.getElementById("e-mail").value; 
+//     let message= document.getElementById("message").value;
 
-    //saving the values in local storage
-    localStorage.setItem("nameValue", name);
-    localStorage.setItem("emailValue", email);
-    localStorage.setItem("messageValue", message);
+//     //saving the values in local storage
+//     localStorage.setItem("nameValue", name);
+//     localStorage.setItem("emailValue", email);
+//     localStorage.setItem("messageValue", message);
+
+//     return;
+// }
+
+function getFormData(e){
+    //no print in url bar
+    e.preventDefault();
+    //get div we're gonna print in
+    const name = document.getElementById('enter-name').value;
+    const email = document.getElementById('e-mail').value;
+    const message = document.getElementById('message').value;
+
+    localStorage.setItem('nameValue', name);
+    localStorage.setItem('emailValue', email);
+    localStorage.setItem('messageValue', message);
 
     return;
 }
 
-// function printSubmittedForms(e){
-//     //no print in url bar
-//     e.preventDefault();
-//     //create div we're gonna print in
-//     let printHere = document.createElement('div');
-//     //create new p element to put the info in
-//     let newContent = document.createElement('p');
-//     //giv p an id and id name
-//     newContent.setAttribute("id", "print-data");
-//     //attach p element to div
-//     printHere.append(newContent);
-//     //print content in p element
-//     newContent.textContent=
-//     `Name: ${localStorage.getItem("nameValue")} 
-//     Email: ${localStorage.getItem("emailValue")} 
-//     Message: ${localStorage.getItem("messageValue")}`;
-//     console.log(newContent);
-// }
+function hasBeenSubmitted(){
+    alert('Your form has been submitted. Thank you!');
+    contactUsForm.reset();
+}
 
-// function hasBeenSubmitted(){
-//     alert('Your form has been submitted. Thank you!');
-//     contactUsForm.reset();
-// }
-
-// contactUsForm.addEventListener('submit', getData);
-// contactUsForm.addEventListener('submit', printSubmittedForms);
-// contactUsForm.addEventListener('submit', hasBeenSubmitted);
-
+contactUsForm.addEventListener('submit', getFormData);
+contactUsForm.addEventListener('submit', hasBeenSubmitted);
